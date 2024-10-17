927c6a1b7cc86cf592617d8860a93e5b1063c181
Neil Cook <neil.cook@noware.co.uk>
2024-10-17 16:27:32 +0100

    Remove more Type tests

--- tests/endpoints/IndicesTest/PutMappingTest.lua
+++ tests/endpoints/IndicesTest/PutMappingTest.lua
@@ -55,33 +55,3 @@ function requestIndexTest()
   assert_not_nil(err)
 end
 
--- Testing name request
-function requestTypeTest()
-  mockTransport.method = "PUT"
-  mockTransport.uri = "/_mapping/user_12"
-  mockTransport.params = {}
-  mockTransport.body = nil
-
-  endpoint:setParams{
-    type = "user_12"
-  }
-
-  local _, err = endpoint:request()
-  assert_nil(err)
-end
-
--- Testing index and name request
-function requestIndexTypeTest()
-  mockTransport.method = "PUT"
-  mockTransport.uri = "/users/_mapping/user_12"
-  mockTransport.params = {}
-  mockTransport.body = nil
-
-  endpoint:setParams{
-    index = "users",
-    type = "user_12"
-  }
-
-  local _, err = endpoint:request()
-  assert_nil(err)
-end
